6c8d2e6db540dbb5afc4baefac68db4ee6f87698
warrre <warren.soulsey@gmail.com>
2024-10-23 12:06:49 +0200

    betty

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main -5 -printf.c
+ * main - 5 -printf.c
  *
  * Return: Always 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art")
-	return 0
+	printf ("with proper grammar, but the outcome is a piece of art;\n");
+	return (0);
 }
-
