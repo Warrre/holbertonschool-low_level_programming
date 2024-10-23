21b9a9ec0b23880d68fada6a9b792601460a5d08
warrre <warren.soulsey@gmail.com>
2024-10-23 17:53:20 +0200

    betty

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - 6-size.c
+ * Description:This program prints the size of various types on the computer it is compiled and run
+ * Return: Always 0
+ */
+int main(void)
+{
+	 printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	 return (0);
+}
