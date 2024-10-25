02f263e71ace0e76eeb9408d3d9fe3a62ce86c1a
warrre <warren.soulsey@gmail.com>
2024-10-25 20:48:01 +0200

    positive

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
+
 /**
- * main - 9-print_comb.c
+ * main - Prints numbers from 0 to 9 with commas and spaces
  *
- * Return: Always returns 0
+ * Description: This function prints all single digit numbers
+ * from 0 to 9 separated by commas and spaces, followed by a new line.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int num;
-	for (num = 0; num <= 9; num++)
 
+	for (num = 0; num <= 9; num++)
 	{
-	putchar('0' + num); /*Print the current number*/
-	/*Check if the current number is not the last one*/
+	putchar('0' + num);
+
 	if (num < 9)
 	{
-	putchar(',');    /*Print the comma*/
-	putchar(' ');    /*Print the space after the comma*/
+		putchar(',');
+		putchar(' ');
 	}
 	}
-	putchar('\n'); /*New line at the end*/
-	return (0); /*Indicate successful execution*/
+
+	putchar('\n');
+
+	return (0);
 }
