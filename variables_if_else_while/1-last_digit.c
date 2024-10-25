ef431a7343ec8e01c7ffe25ba6d1ee0ad3fa3c29
warrre <warren.soulsey@gmail.com>
2024-10-25 09:54:13 +0200

    positive

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,15 +9,17 @@
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
 
-	if (n > 0)
+	if (last_digit > 5)
 	{
 		printf("%d and is greater than 5\n", n);
 	}
-	else if (n == 0)
+	else if (last_digit == 0)
 	{
 		printf("%d and is 0\n", n);
 	}
