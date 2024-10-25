515cbfd2156fc806236f870157b868665a347c5a
warrre <warren.soulsey@gmail.com>
2024-10-25 11:06:29 +0200

    positive

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -25,7 +25,9 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("The last digit of %d is %d and ", n, last_digit);
+		printf("is greater than 5\n");
+
 	}
 	return (0);
 }
