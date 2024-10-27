6233f5dc36bb6214c31935eac0876e1dc2c216c0
warrre <warren.soulsey@gmail.com>
2024-10-27 17:36:31 +0100

    putchar

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,17 @@
  *
  * Return: Always returns 0
  */
-void print_alphabet_x10(void) {
+void print_alphabet_x10(void)
+{
 	int i;
 	char letter;
 
-	for (i = 0; i < 10; i++) {
-	for (letter = 'a'; letter <= 'z'; letter++) {
-	_putchar(letter);
+	for (i = 0; i < 10; i++)
+	{/*Loop 10 times*/
+	for (letter = 'a'; letter <= 'z'; letter++)
+{
+	_putchar(letter); /*Print each letter*/
 	}
-	_putchar('\n');
+	_putchar('\n'); /*Print a new line after each alphabet*/
 	}
 }
