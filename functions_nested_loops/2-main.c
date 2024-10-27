3f8053e680ee7984238fc2cfa67255db0cad1867
warrre <warren.soulsey@gmail.com>
2024-10-27 18:31:02 +0100

    putchar

--- functions_nested_loops/2-main.c
+++ functions_nested_loops/2-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-int main(void) {
+int main(void)
+{
 	print_alphabet_x10();
-	return 0;
+	return (0);
 }
--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 /**
  * main - 2-print_alphabet_x10.c
  *
@@ -8,12 +9,12 @@ void print_alphabet_x10(void)
 {
 	int i;
 	char letter;
-	for (i = 0; i < 10; i++)
+		for (i = 0; i < 10; i++)
 	{
-	for (letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 	{
-	_putchar(letter);
+		_putchar(letter);
 	}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
