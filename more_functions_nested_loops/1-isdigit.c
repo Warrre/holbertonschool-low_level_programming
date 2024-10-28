9bd7df53ec24515b27b313aaf1a333dab6ff9d40
warrre <warren.soulsey@gmail.com>
2024-10-28 14:39:00 +0100

    isupper

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /*
  * _isupper - 1-isdigit.c
- *
+ *@c: The character to be checked
  * Return: 1 if c is uppercase, 0 etheraise
  */
-int _isdigit(int c)
+int _isdigit(int c);
 {
 	if (c >= '0' && c <= '9')
 		return (1);
