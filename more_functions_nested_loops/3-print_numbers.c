29a77427563f108846ccefe0ca5708c433f2d467
warrre <warren.soulsey@gmail.com>
2024-10-29 11:05:47 +0100

    isupper

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * putchar - 3-print_numbers.c
+ * print_numbers - Prints the numbers from 0 to 9, followed by a new line
+ *
  * Return: void
  */
 void print_numbers(void)
