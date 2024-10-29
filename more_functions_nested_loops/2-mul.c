bd478de970544e2443f94a318574bd4fb9d2c07b
warrre <warren.soulsey@gmail.com>
2024-10-29 09:44:50 +0100

    isupper

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul.c - 2-mul.c
+ *
+ * @c: The character to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int main(void)
+{
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+	return (0);
+}
