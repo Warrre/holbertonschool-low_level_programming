1c9b728b9bec4ad5895bcd8d1e32698609f19491
warrre <warren.soulsey@gmail.com>
2024-11-02 13:39:16 +0100

    isupper

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,33 @@
 #include "main.h"
 
 /**
- * main - 7-print_diagonal.c
- * Description: This function prints a straight line using the '_' character.
- * The length of the line is determined by the input parameter 'n'.
- * @n: The number of times the character '/' should be printed
- * f n is 0 or less, only a newline character is printed.
+ * print_diagonal - Draws a diagonal line in the terminal
+ * @n: The number of times the character '\' should be printed
+ *
+ * Description: This function prints a diagonal line using the '\' character.
+ * The length of the diagonal is determined by the input parameter 'n'.
+ * If n is 0 or less, only a newline character is printed.
+ *
  * Return: void
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n <= 0)
 	{
-	_putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
-	for (i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 	{
-	_putchar('/');
+		for (j = 0; j < i; j++)
+	{
+		_putchar(' ');
 	}
-	_putchar('\n');
+		_putchar('\\');
+		_putchar('\n');
+	}
 	}
 }
