d32d29a62f6b1f9b9799e3cc40b455a9d758ece5
warrre <warren.soulsey@gmail.com>
2024-11-03 19:00:43 +0100

    pointers

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,4 +1,12 @@
 #include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
 
 /**
  * _puts - prints a string, followed by a new line, to stdout
@@ -8,8 +16,8 @@ void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-	_putchar(*str);
-	str++;
+		_putchar(*str);
+		str++;
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
