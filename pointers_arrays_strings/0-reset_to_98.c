09e6e6383b0d8bd79b3bac2081a9e31478068ad5
warrre <warren.soulsey@gmail.com>
2024-11-03 16:45:56 +0100

    pointers

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include "main.h"
 
-void reset_to_98(int *n);
-
-int main(void)
+/**
+ * reset_to_98 - updates the value pointed to by n to 98
+ * @n: pointer to an integer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
 {
-	int n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
+    *n = 98;
 }
