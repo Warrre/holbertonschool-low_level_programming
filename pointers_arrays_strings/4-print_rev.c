c9ee984369ba625f682e6605ad3bdef3b8ddf07d
warrre <warren.soulsey@gmail.com>
2024-11-03 19:15:33 +0100

    pointers

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,15 @@
 #include "main.h"
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
  * print_rev - prints a string in reverse, followed by a new line
  * @s: pointer to the string to print
  */
@@ -12,15 +21,15 @@ void print_rev(char *s)
 	/* First, calculate the length of the string */
 	while (s[length] != '\0')
 	{
-		length++;
+	length++;
 	}
 
 	/* Then, print the string in reverse */
 	for (i = length - 1; i >= 0; i--)
 	{
-		_putchar(s[i]);
+	_putchar(s[i]);
 	}
 
 	/* Print a new line at the end */
-		_putchar('\n');
+	_putchar('\n');
 }
