4c6b9d26e012db62be3fa0a6564fe861f41db762
warrre <warren.soulsey@gmail.com>
2024-11-16 22:13:48 +0100

    ore

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,8 +3,9 @@
 /**
  * malloc_checked - Allocates memory using malloc
  * @b: The number of bytes to allocate
+ *
  * Return: A pointer to the allocated memory
- * If malloc fails, terminates process with status 98
+ * If malloc fails, terminates process with status value 98
  */
 void *malloc_checked(unsigned int b)
 {
