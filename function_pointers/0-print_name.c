8a3ed107bc271e40403eee9dd79727f2b53ef06f
warrre <warren.soulsey@gmail.com>
2024-11-21 21:10:33 +0100

    name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
-#include <stddef.h>
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * print_name - prints a name using a given function
  * @name: name to be printed
@@ -10,6 +10,5 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-
 	f(name);
 }
