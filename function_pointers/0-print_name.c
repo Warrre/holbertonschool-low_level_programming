8d53f01e6792c82c8b54944a240d8abe726777ec
warrre <warren.soulsey@gmail.com>
2024-11-27 22:54:33 +0100

    style: improve formatting by removing unnecessary whitespace in print_name function

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
-#include "function_pointers.h"
-#include <stddef.h>
+#include"function_pointers.h"
+#include<stddef.h>
 /**
  * print_name - prints a name using a given function
  * @name: name to be printed
  * @f: function pointer to the printing function
- *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
