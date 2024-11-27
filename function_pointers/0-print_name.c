cdf118e035a0d97aa4e9720f1991dc47df263fcb
warrre <warren.soulsey@gmail.com>
2024-11-27 22:45:56 +0100

    refactor: rename and simplify print_name function to accept a function pointer

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,30 +1,14 @@
 #include "function_pointers.h"
 #include <stddef.h>
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
+ * print_name - prints a name using a given function
+ * @name: name to be printed
+ * @f: function pointer to the printing function
  *
  * Return: Nothing.
  */
-void print_name_as_is(char *name)
+void print_name(char *name, void (*f)(char *))
 {
-	printf("Hello, my name is %s\n", name);
-}
-void print_name_uppercase(char *name)
-{
-	unsigned int i;
-	for (i = 0; name[i] != '\0'; i++)
-	{
-		if (name[i] >= 97 && name[i] <= 122)
-		{
-			name[i] = name[i] - 32;
-		}
-	}
-	printf("Hello, my uppercase name is %s\n", name);
-}
-int main(void)
-{
-	print_name("Danton", print_name_as_is);
-	print_name("Rick Harris", print_name_uppercase);
-	return (0);
+	if (name && f)
+		f(name);
 }
