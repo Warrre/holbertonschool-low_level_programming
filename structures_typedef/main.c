5726dee1c99e31e1d65d6827878c8874fef37734
warrre <warren.soulsey@gmail.com>
2024-12-04 23:28:04 +0100

    fix: add function prototypes for dog management in main.c

--- structures_typedef/main.c
+++ structures_typedef/main.c
@@ -3,7 +3,12 @@
 
 int main(void)
 {
-    struct dog my_dog;
-    
-    return (0);
+
+struct dog my_dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+return (0);
 }
