9c3b6002ea87000920971c4b5f3447688fc01214
warrre <warren.soulsey@gmail.com>
2024-12-04 23:18:35 +0100

    fix: add missing newline at end of dog.h and correct struct closing brace

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - dog structure
  * @name: name of the dog
@@ -13,6 +14,7 @@ struct dog
     char *name;
     float age;
     char *owner;
-};
+}
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
