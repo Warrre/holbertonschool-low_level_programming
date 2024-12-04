cc96d2537fe8805853e190f401388e8c410af39a
warrre <warren.soulsey@gmail.com>
2024-12-04 22:43:36 +0100

    fix: add null check in init_dog function and remove erroneous return statement

--- structures_typedef/structures_typedef/1-init_dog.c
+++ structures_typedef/structures_typedef/1-init_dog.c
@@ -1,21 +1,21 @@
 #include "dog.h"
 #include <stddef.h>
+#include <stdlib.h>
+
 /**
  * init_dog - initializes a variable of type struct dog
  * @d: pointer to the struct dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- *
- * Return: void
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
     if (d == NULL)
+        return;
 
     d->name = name;
     d->age = age;
     d->owner = owner;
 }
-Return (0);
+```
