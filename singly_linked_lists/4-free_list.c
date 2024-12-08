f9812f84855f5349139a74517b9c1ef67d3656f7
warrre <warren.soulsey@gmail.com>
2024-12-08 12:02:47 +0100

    fix: improve null check in free_list function for better safety

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -9,11 +9,11 @@ void free_list(list_t *head)
 {
     list_t *temp;
 
-    while (head)
+    while (head != NULL)
     {
-        temp = head->next;
-        free(head->str);
-        free(head);
-        head = temp;
+        temp = head;
+        head = head->next;
+        free(temp->str);
+        free(temp);
     }
 }
