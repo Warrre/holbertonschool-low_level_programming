24ea3a9a40c2c18128276a8804d50de4feb2f533
warrre <warren.soulsey@gmail.com>
2024-12-10 00:22:41 +0100

    refactor: update print_binary function documentation and improve readability

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,13 +1,14 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
- * print_binary - Imprime la représentation binaire d'un nombre
- * @n: Le nombre à imprimer en binaire
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to print in binary.
  */
 void print_binary(unsigned long int n)
 {
     if (n > 1)
-        print_binary(n >> 1);
+        print_binary(n >> 1);  // Shift right to process the next bit
 
-    _putchar((n & 1) + '0');
+    _putchar((n & 1) + '0');  // Print the least significant bit
 }
