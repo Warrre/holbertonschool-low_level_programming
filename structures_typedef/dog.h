3717bc66522e6fe760c4bc09a49e421d2d475b34
warrre <warren.soulsey@gmail.com>
2024-12-09 21:49:29 +0100

    docs: add documentation for dog structure in dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - Structure représentant un chien
+ * @name: Nom du chien
+ * @age: Âge du chien
+ * @owner: Propriétaire du chien
+ *
+ * Description: Cette structure contient les informations de base d'un chien
+ */
 struct Dog
 {
     char *name;
