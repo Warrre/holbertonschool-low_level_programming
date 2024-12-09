faef5dd20e6858817de24590b7f62c0917eedb2c
warrre <warren.soulsey@gmail.com>
2024-12-09 22:14:15 +0100

    docs: improve comments for struct dog for clarity and consistency

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,18 +2,17 @@
 #define DOG_H
 
 /**
- * struct dog - A dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
- *
- * Description: Longer description
+ * struct dog - dog's basic info
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Description: longer description
  */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
 
 #endif
