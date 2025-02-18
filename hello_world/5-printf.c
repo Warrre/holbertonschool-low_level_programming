57acb37bc97f282e8cce17128548be6fa30c9a27
warrre <warren.soulsey@gmail.com>
2025-02-18 14:44:28 +0100

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main-entré
+ *main - print second sentence
  *
  *Description:afficher suivi d'un saut a la ligne
  *
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	printf:(with proper grammar, but the outcome is a piece of art"/n")
-	return(0)
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
 }
