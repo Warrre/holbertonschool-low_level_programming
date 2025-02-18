c481c3c00f209026ea76946379b29d415549d43c
warrre <warren.soulsey@gmail.com>
2025-02-18 14:19:38 +0100

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,12 @@
-#!/bin/bash
-puts: with proper grammar, but the outcome is a piece of art
-(\n)
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Prints about programming
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
