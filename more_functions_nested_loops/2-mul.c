d4edbec2c93d0bfc8aca65dfca2c07d8a906ee20
warrre <warren.soulsey@gmail.com>
2025-02-26 18:03:39 +0100

    fix: correct function name and improve documentation in multiplication function

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * multy - multiplication
+ * mult - multiplication
  * @; first digit
  * @: second
  * Rturn: result
  */
 int mul(int a, int b)
 {
-    return (a * b);
+return (a * b);
 }
