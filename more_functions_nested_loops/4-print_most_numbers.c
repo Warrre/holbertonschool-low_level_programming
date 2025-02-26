54e9a929da4dab4d09c53701d41f8be102dfd7be
warrre <warren.soulsey@gmail.com>
2025-02-26 18:41:20 +0100

    feat: add print_most_numbers function to print digits 0 to 9 fix: refactor _isupper function for improved readability

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * Write a function that prints the numbers,
+ * from 0 to 9, followed by a new line
+ */
+void print_most_numbers(void)
+{
+int n;
+
+for ( n = '0'; n <= '9'; n++)
+{
+_putchar (n)
+}
+{
+_putchar('\n')
+}
+}
