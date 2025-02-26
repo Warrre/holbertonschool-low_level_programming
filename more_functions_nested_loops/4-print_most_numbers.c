8a5e9c10256e2cd494cee7c0fa664e9eeb52da21
warrre <warren.soulsey@gmail.com>
2025-02-26 18:49:21 +0100

    fix: improve formatting and consistency in print_most_numbers function

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,18 +2,18 @@
 /**
  * Write a function that prints the numbers,
  * from 0 to 9, followed by a new line
+ * Do not print 2 and 4
  */
 void print_most_numbers(void)
 {
 int n;
-for ( n = '0'; n <= '9'; n++)
+
+for (n = '0'; n <= '9'; n++)
 {
 if (n != '2' && n != '4')
-}
 {
-_putchar (n);
+_putchar(n);
 }
-{
+}
 _putchar('\n');
-}
 }
