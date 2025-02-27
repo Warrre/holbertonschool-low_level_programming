7dc69d64dcc83e5cfd0ba6711e7fbbccafd9eac0
warrre <warren.soulsey@gmail.com>
2025-02-27 01:14:50 +0100

    fix: improve code formatting and consistency in more_numbers function

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,24 +5,24 @@
 
 void more_numbers(void)
 {
-	int n;
-	int count;
-	int tens;
-	int ones;
+int n;
+int count;
+int tens;
+int ones;
 
-	for (count = 0; count <= 9; count++)
-	{
-	for (n = 0; n <= 14; n++)
-	{
-	tens = n / 10;
-	ones = n % 10;
+for (count = 0; count <= 9; count++)
+{
+for (n = 0; n <= 14; n++)
+{
+tens = n / 10;
+ones = n % 10;
 
-	if (n > 9)
-	{
-		_putchar(tens + '0');
-	}
-	_putchar(ones + '0');
-	}
-	_putchar('\n');
-	}
+if (n > 9)
+{
+_putchar(tens + '0');
+}
+_putchar(ones + '0');
+}
+_putchar('\n');
+}
 }
