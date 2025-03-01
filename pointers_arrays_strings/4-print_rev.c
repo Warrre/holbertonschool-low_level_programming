affff5595bfddfad9d251b74118065babecbffa8
warrre <warren.soulsey@gmail.com>
2025-03-01 17:06:19 +0100

    fix: remove unnecessary semicolon in print_rev function for correct loop execution

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,8 +8,7 @@ void print_rev(char *s)
 {
 int length = 0;
 
-for (length = 0; s[length] != '\0'; length++)
-;
+for (length = 0; s[length] != '\0'; length++);
 
 for (length--; length >= 0; length--)
 {
