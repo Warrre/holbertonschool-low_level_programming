02dbac41717d326f97a0d2e7ecb4b531dc3ff791
warrre <warren.soulsey@gmail.com>
2025-03-01 16:13:36 +0100

    fix: remove trailing whitespace in function documentation for reset_to_98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98 - Updates the value of an integer to 98
  * @n: Pointer to the integer to modify
  *
- * Description: This function takes a pointer to an integer as a parameter  
+ * Description: This function takes a pointer to an integer as a parameter
  * and updates its value to 98.
  */
 void reset_to_98(int *n)
