ceea5ccd8017fded76ba23d4bfc795b3b91b3dfe
warrre <warren.soulsey@gmail.com>
2025-03-01 16:44:37 +0100

    fix: update _puts function to improve string printing and add detailed documentation

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,13 +1,18 @@
 #include "main.h"
 /**
- * _puts - print a string
- * @str: string to print
+ * _puts - Prints a string followed by a new line to stdout
+ * @str: Pointer to the string to be printed
+ *
+ * Description: This function iterates through the string and prints
+ * each character using _putchar, then prints a newline character.
  */
 void _puts(char *str)
 {
-for (; *str != '\0'; str++)
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
 {
-_putchar(*str);
+ _putchar(str[i]);
 }
-_putchar('\n');
+ _putchar('\n');
 }
