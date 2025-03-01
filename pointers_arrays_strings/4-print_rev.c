59cc732a26687e83ef88ba292ad7acbec9a77b2a
warrre <warren.soulsey@gmail.com>
2025-03-01 17:05:43 +0100

    fix: remove redundant comments in print_rev function for clarity

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,9 @@ void print_rev(char *s)
 {
 int length = 0;
 
-// Find the length of the string
 for (length = 0; s[length] != '\0'; length++)
 ;
 
-// Print the string in reverse order using a for loop
 for (length--; length >= 0; length--)
 {
 _putchar(s[length]);
