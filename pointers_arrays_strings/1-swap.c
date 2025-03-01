1344d5ef7ce3ca780f6a85f07b9d159ce33ce5f9
warrre <warren.soulsey@gmail.com>
2025-03-01 16:16:54 +0100

    feat: add swap_int function to swap two integer values

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - swap two values
+ * @a: value 1
+ * @b: value 2
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+
+c = *a;
+*a = *b;
+*b = c;
+}
