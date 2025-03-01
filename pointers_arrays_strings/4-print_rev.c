1036c4f4c4d0bbec602cf364d641829808ab6cab
warrre <warren.soulsey@gmail.com>
2025-03-01 17:11:56 +0100

    fix: add documentation comment for print_rev function and correct loop syntax

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,14 @@
 #include "main.h"
-
+/**
+ * print_rev - Prints a string in reverse followed by a new line
+ * @s: Pointer to the string to be printed
+ */
 void print_rev(char *s)
 {
 int length = 0;
 
-for (length = 0; s[length] != '\0'; length++);
+for (length = 0; s[length] != '\0'; length++)
+;
 
 for (length--; length >= 0; length--)
 {
