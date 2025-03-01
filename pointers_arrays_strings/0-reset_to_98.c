099820833b53090c892a390c93fd70d175f0e01e
warrre <warren.soulsey@gmail.com>
2025-03-01 16:09:40 +0100

    fix: remove extra newline in function documentation comment in reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * Write a function that takes a pointer to an int 
+ * Write a function that takes a pointer to an int
  * as parameter and updates the value it points to to 98.
  */
 void reset_to_98(int *n)
