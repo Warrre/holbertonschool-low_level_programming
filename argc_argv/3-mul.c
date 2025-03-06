94bcc1865ad8a9270cbb09cd841fdb6cfb93bad6
warrre <warren.soulsey@gmail.com>
2025-03-06 23:13:33 +0100

    fix: improve error handling and validation in multiplication program

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,17 +1,16 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
 /**
- * is_number - Vérifie si une chaîne ne contient que des chiffres
- * @str: La chaîne à vérifier
- * Return: 1 si c'est un nombre, 0 sinon
+ * is_number - Checks if a string contains only digits
+ * @str: The string to check
+ * Return: 1 if it's a number, 0 otherwise
  */
 int is_number(char *str)
 {
 int i = 0;
-if (str[i] == '-' || str[i] == '+') /* Gérer les nombres négatifs */
+if (str[i] == '-' || str[i] == '+') /* Gérer les nombres négatifs/positifs */
 i++;
 while (str[i])
 {
@@ -23,31 +22,24 @@ return (1);
 }
 
 /**
- * main - Fonction principale
- * @argc: Nombre d'arguments
- * @argv: Tableau des arguments
- * Return: 0 en cas de succès, 1 sinon
+ * main - Multiplies two numbers
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: 0 if success, 1 if error
  */
 int main(int argc, char *argv[])
 {
 int num1, num2, result;
 
-if (argc != 3)
+if (argc != 3 || !is_number(argv[1]) || !is_number(argv[2]))
 {
-printf("Usage: %s <num1> <num2>\n", argv[0]);
-return (1);
-}
-
-if (!is_number(argv[1]) || !is_number(argv[2]))
-{
-printf("Error: Both arguments must be integers.\n");
+printf("Error\n");
 return (1);
 }
 
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 result = num1 * num2;
-
 printf("%d\n", result);
 return (0);
 }
