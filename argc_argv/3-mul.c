20b630ce61a804b962a92d5158a0e97404b21cde
warrre <warren.soulsey@gmail.com>
2025-03-06 23:10:01 +0100

    feat: add input validation for command line arguments in multiplication program

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,29 +1,53 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - function where the program starts
- * @argc: size of argv and number of command line arguments
- * @argv: array containing the program command line arguments
- * Return: 0 always success
+ * is_number - Vérifie si une chaîne ne contient que des chiffres
+ * @str: La chaîne à vérifier
+ * Return: 1 si c'est un nombre, 0 sinon
  */
+int is_number(char *str)
+{
+int i = 0;
+if (str[i] == '-' || str[i] == '+') /* Gérer les nombres négatifs */
+i++;
+while (str[i])
+{
+if (!isdigit(str[i]))
+return (0);
+i++;
+}
+return (1);
+}
 
+/**
+ * main - Fonction principale
+ * @argc: Nombre d'arguments
+ * @argv: Tableau des arguments
+ * Return: 0 en cas de succès, 1 sinon
+ */
 int main(int argc, char *argv[])
 {
-int result;
-int num1, num2;
+int num1, num2, result;
 
 if (argc != 3)
 {
-printf("Error\n");
+printf("Usage: %s <num1> <num2>\n", argv[0]);
+return (1);
+}
+
+if (!is_number(argv[1]) || !is_number(argv[2]))
+{
+printf("Error: Both arguments must be integers.\n");
 return (1);
 }
 
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 result = num1 * num2;
-printf("%d\n", result);
 
+printf("%d\n", result);
 return (0);
 }
