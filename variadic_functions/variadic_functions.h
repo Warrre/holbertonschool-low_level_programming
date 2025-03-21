e537ed13a60f02f531b21f2c3255c58c14f5fdfb
warrre <warren.soulsey@gmail.com>
2025-03-21 12:55:16 +0100

    feat: add header file for variadic functions and define format struct

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,16 @@
+#ifndef VAR_H
+#define VAR_H
+#include <stdarg.h>
+/**
+ * struct format - format for miniprintf
+ * @fo: format character (c, i, f, s)
+ * @f: function to use
+ */
+typedef struct format
+{
+	char *fo;
+	void (*f)(char *separator, va_list ap);
+} form;
+
+int sum_them_all(const unsigned int n, ...);
+#endif
