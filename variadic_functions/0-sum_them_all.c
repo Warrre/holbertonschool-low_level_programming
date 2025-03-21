b7d035fb135fc453ed0886b8ca6b812895e91b8f
warrre <warren.soulsey@gmail.com>
2025-03-21 13:04:35 +0100

    feat: refactor sum_them_all function and add _putchar utility function

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,24 +1,30 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
+#include <stdio.h>
+
 /**
- * sum_them_all - add all args
- * @n: ints to add
- * Return: Result
+ * sum_them_all - returns the sum of all its parameters
+ * @n: number of arguments
+ * Return: the difference of all its parameters
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-va_list list;
-unsigned int i = 0, sum = 0;
+	va_list ap;
+	unsigned int i;
+	int sum = 0;
 
-if (n == 0)
-return (0);
-va_start(list, n);
+	if (n == 0)
+	{
+		return (0);
+	}
 
-while (i < n)
-{
-sum += va_arg(list, int);
-i++;
-}
-va_end(list);
-return (sum);
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+
+	va_end(ap);
+	return (sum);
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,17 +1,22 @@
-#ifndef VAR_H
-#define VAR_H
-#include <stdarg.h>
-/**
- * struct format - format for miniprintf
- * @fo: format character (c, i, f, s)
- * @f: function to use
- */
-typedef struct format
-{
-	char *fo;
-	void (*f)(char *separator, va_list ap);
-} form;
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/**
+ * struct print - Struct print
+ * @type: The type of argument
+ * @f: The function associated
+ */
+
+typedef struct print
+{
+	char *type;
+	void (*f)(va_list);
+} print_t;
+
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 
 #endif
