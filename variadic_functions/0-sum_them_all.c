2d5a5bf46777c81026b13e69f73ebe7e7ea3ae32
warrre <warren.soulsey@gmail.com>
2025-03-21 12:53:18 +0100

    feat: add sum_them_all function to calculate the sum of all arguments

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - add all args
+ * @n: ints to add
+ * Return: Result
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i = 0, sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(list, n);
+
+	while (i < n)
+	{
+	sum += va_arg(list, int);
+	i++;
+	}
+	va_end(list);
+	return (sum);
+}
