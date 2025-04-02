18feaa030ca7819a37f991f9a57276e03e9533c8
warrre <warren.soulsey@gmail.com>
2025-04-02 23:25:31 +0200

    Refactor list_len function for improved readability and efficiency

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,25 +1,20 @@
 #include "lists.h"
-#include <stdlib.h>
-#include "lists.h" 
+#include <stdio.h>
+#include <stddef.h>
 /**
  * list_len - returns the number of elements in a linked list_t list
- * @h: pointer to the list_t list
+ * @h: pointer to the head of the list
  * Return: the number of nodes
  */
-
 size_t list_len(const list_t *h)
 {
-	int count = 0;
+	size_t count = 0;
+	const list_t *current = h;
 
-	if (h == NULL)
+	while (current)
 	{
-		return (0);
-	}
-
-	while (h != NULL)
-	{
-		h = h->next;
 		count++;
+		current = current->next;
 	}
 	return (count);
 }
