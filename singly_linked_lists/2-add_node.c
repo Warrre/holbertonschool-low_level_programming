ae92af295c025d92f5d607b631dbcda6dab37e95
warrre <warren.soulsey@gmail.com>
2025-04-02 15:37:07 +0200

    Implement singly linked list functions: add_node, add_node_end, print_list, list_len, and free_list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to the list_t list
+ * @str: string to be added to the list_t list
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	int i = 0;
+
+	new_node = malloc(sizeof(list_t));
+
+	if (head == NULL || new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	new_node->len = i;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
