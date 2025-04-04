26924d6c334671679b3ec5e85c2402f66004a728
warrre <warren.soulsey@gmail.com>
2025-04-04 15:19:42 +0200

    Fix formatting by adjusting indentation and adding a newline at the end of dlistint_len function

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -7,19 +7,19 @@
  */
 
  size_t dlistint_len(const dlistint_t *h)
- {
-     int count = 0;
- 
-     if (h == NULL)
-     {
-         return (0);
-     }
- 
-     while (h != NULL)
-     {
-         h = h->next;
-         count++;
-     }
-     return (count);
- }
+{
+    int count = 0;
+
+    if (h == NULL)
+    {
+        return (0);
+    }
+
+    while (h != NULL)
+    {
+        h = h->next;
+        count++;
+    }
+    return (count);
+}
  
