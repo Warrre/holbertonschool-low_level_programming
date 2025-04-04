8798b1c37bc6b7d59fcac1bf835015bbf2613999
warrre <warren.soulsey@gmail.com>
2025-04-04 15:10:11 +0200

    Fix formatting and improve comments in print_dlistint function

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,25 +1,22 @@
 #include "lists.h"
 #include <stdio.h>
-/**
- * print_dlistint - prints all the elements of a dlistint_t list
- * @h: pointer to the dlistint_t list
- * Return: the number of nodes
- */
 
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count = 0;
+    size_t count = 0;
 
-	if (h == NULL)
-	{
-		return (0);
-	}
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        count++;
+    }
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
-	}
-	return (count);
+    return (count);
 }
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stddef.h>
 /**
  * struct dlistint_s - doubly linked list
